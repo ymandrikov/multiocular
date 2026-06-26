e6039793f3810b97d724cbcc4b829e98a3ef39fe
Andrey Sitnik <andrey@sitnik.es>
2026-06-26 09:06:30 +0000

    Move from Prettier to oxfmt

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,5 +4,5 @@ files=$(git ls-files --modified --deleted --others --exclude-standard \
   -- '*.ts' '*.js' '*.cts' '*.mts' '*.cjs' '*.mjs' '*.svelte')
 if [[ -n "$files" ]]; then
   echo "$files" | xargs pnpm eslint --cache --concurrency auto --fix
-  echo "$files" | xargs pnpm prettier --experimental-cli --write
+  echo "$files" | xargs pnpm oxfmt
 fi
